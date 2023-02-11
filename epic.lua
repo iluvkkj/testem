3df9142b5179752f0b970d1801bd5cc59bae3de2
iluvkkj <122802954+iluvkkj@users.noreply.github.com>
2023-02-11 12:14:08 -0300

    v2

--- epic.lua
+++ epic.lua
@@ -7,13 +7,7 @@
 ]]--
 
 local scriptUrl = ({
-    [277751860] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/Gr.lua", -- lvl1-2 277751860
-    [277751860] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/Gr.lua", -- lvl3 277751860
-    [277751860] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/Gr.lua", -- lvl4 277751860
-    [277751860] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/Gr.lua", -- lvl5 277751860
-    [277751860] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/Gr.lua", -- lvl6 277751860
-    [277751860] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/Gr.lua", -- lvl7 277751860 
-    [277751860] = "https://raw.githubusercontent.com/EzydoesGithub/EzyWare/main/Gr.lua", -- lvl8 277751860
+    [277751860] = "https://raw.githubusercontent.com/SlamminPig/rblxgames/main/Epic%20Minigames/EpicMinigamesGUI", -- lvl1-2 277751860
 })[game.PlaceId]
 
 if scriptUrl then
